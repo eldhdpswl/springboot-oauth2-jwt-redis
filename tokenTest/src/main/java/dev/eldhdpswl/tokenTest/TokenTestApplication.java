890326b102e75e9d75ccc310a434cd858d149515
신동금 <eldhdpswl@gmail.com>
2022-05-13 00:09:24 +0900

    jwt study commit

---
+++ tokenTest/src/main/java/dev/eldhdpswl/tokenTest/TokenTestApplication.java
@@ -0,0 +1,13 @@
+package dev.eldhdpswl.tokenTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenTestApplication.class, args);
+	}
+
+}
---
+++ tokenTest/src/test/java/dev/eldhdpswl/tokenTest/TokenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eldhdpswl.tokenTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
